d179983f1f1fbdfbdbcf3e72444d01df1c607c1e
Dawson <Jade.Dawson@gov.bc.ca>
2024-03-04 20:09:30 -0800

    updating name for plit, ellipse calcs, new csv

--- scripts/calculate_ellipse_extents.R
+++ scripts/calculate_ellipse_extents.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-dat = read.csv("JD-data_Feb16.csv") %>%
+dat = read.csv("ellipse-data_Mar04.csv") %>%
   filter(TAXON %in% c("SMB", "YPERCH"))
 
-p = ggplot(dat, aes(x = d13C, y = d15N, group = paste(Water, TAXON), colour = paste(Water, TAXON))) +
+p = ggplot(dat, aes(x = plit_bound, y = tp_2, group = paste(Water, TAXON), colour = paste(Water, TAXON))) +
   geom_point() + 
   stat_ellipse(type = "norm", level = 0.4)
 p
@@ -16,7 +16,7 @@ ggplot(ellipse_data, aes(x = x, y = y, colour = as.factor(group))) +
   geom_point() 
 
 # calculate min and max
-d13c_d15n_ranges =
+plit_tp_ranges =
   ellipse_data %>%
   group_by(group) %>%
   summarise(d13c_min = min(x),
@@ -32,5 +32,5 @@ name_df = data.frame(
 name_df$group = row.names(name_df)
 
 # merge names to d13c and d15n ranges
-d13c_d15n_ranges = merge(d13c_d15n_ranges, name_df, by = "group")
+plit_tp_ranges = merge(plit_tp_ranges, name_df, by = "group")
   
